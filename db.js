4dc6436b29b8cd95fd1053e1aadfbea6027ac399
--replace-all <diaconu.codrin@gmail.com>
2016-01-31 17:19:12 -0500

    Added pushed to heroku and added postgres database check.

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    "dialect": "sqlite",
-    "storage": __dirname + "/data/dev-todo-api.sqlite"
-});
+//If the code runs on Heroku the env will be set to production else we are setting it to "development". process.env.NODE_ENV === "production" if on Heroku.
+var env = process.env.NODE_ENV || "development";
+
+//If we are on Heroku then access the postgres database, else the sqlite one.
+var sequelize;
+if (env === "production") {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        "dialect": "postgres"
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        "dialect": "sqlite",
+        "storage": __dirname + "/data/dev-todo-api.sqlite"
+    });
+}
 
 var db = {};
 
